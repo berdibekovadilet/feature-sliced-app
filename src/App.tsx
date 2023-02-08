51a51ae4d7a00ab2e636954854383db7d13709a2
Adilet Berdibekov <berdibekovadilet@gmail.com>
2023-02-08 20:52:53 +0600

    added Routing, code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import styles from "./app.module.scss";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+import { Suspense } from "react";
 
 const App = () => {
   return (
     <div className="app">
-      App
-      <button className={styles.btn}>Adilet</button>
+      <Link to="/">Главная</Link>
+      <Link to="/about">About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageLazy />} />
+          <Route path={"/"} element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
