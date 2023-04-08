f1f37c6970d2927d5447f4c13ac1acd8b378a1f5
Adilet Berdibekov <berdibekovadilet@gmail.com>
2023-04-08 14:57:00 +0600

    Added ThemeSwitcher and Button, also swgr and fileloader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,18 @@
-import { AppRouter } from "./providers/router";
-import { useTheme } from "./providers/ThemeProvider";
-import { classNames } from "shared/lib/classNames/classNames";
-import { Navbar } from "widgets/Navbar";
+import {AppRouter} from "./providers/router";
+import {useTheme} from "./providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {Navbar} from "widgets/Navbar";
 import "./styles/index.scss";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+    const {theme} = useTheme();
 
-  return (
-    <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-      <AppRouter />
-      <button onClick={toggleTheme}>Toggle</button>
-    </div>
-  );
+    return (
+        <div className={classNames("app", {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
+        </div>
+    );
 };
 
 export default App;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,24 +1,25 @@
-import { classNames } from "shared/lib/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
-import AppLink, { AppLinkTheme } from "shared/ui/AppLinks/AppLink";
+import AppLink, {AppLinkTheme} from "shared/ui/AppLinks/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
-  className?: string;
+    className?: string;
 }
 
-const Navbar = ({ className }: NavbarProps) => {
-  return (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links}>
-        <AppLink to="/" className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>
-          Главная
-        </AppLink>
-        <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
-          About
-        </AppLink>
-      </div>
-    </div>
-  );
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher/>
+            <div className={cls.links}>
+                <AppLink to="/" className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>
+                    Главная
+                </AppLink>
+                <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
+                    About
+                </AppLink>
+            </div>
+        </div>
+    );
 };
 
-export default Navbar;
